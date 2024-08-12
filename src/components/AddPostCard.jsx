93b5056de729246cc01c749c0794e6c9cbfe7504
mr-atuzie <user email>
2024-08-12 08:10:47 +0100

    photogrid

--- src/components/AddPostCard.jsx
+++ src/components/AddPostCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../redux/features/userSlice";
+import ErrorCard from "./ErrorCard";
 // import PhotoGrid from "./PhotoGrid";
 
 const AddPostCard = ({ setAddPost }) => {
@@ -8,6 +9,7 @@ const AddPostCard = ({ setAddPost }) => {
 
   const [addedPhotos, setAddedPhotos] = useState([]);
   const [desc, setDesc] = useState("");
+  const [errMsg, setErrMsg] = useState("");
 
   async function uploadPhoto(ev) {
     let uploadFiles = [];
@@ -17,9 +19,13 @@ const AddPostCard = ({ setAddPost }) => {
     console.log(files);
 
     for (let i = 0; i < files.length; i++) {
-      uploadFiles.push(URL.createObjectURL(ev.target.files[0]));
+      uploadFiles.push(URL.createObjectURL(ev.target.files[i]));
 
-      console.log(uploadFiles);
+      if (files.length > 4) {
+        return setErrMsg("You can only add 4 photos");
+      }
+
+      // console.log(uploadFiles);
     }
 
     setAddedPhotos((prev) => {
@@ -31,7 +37,7 @@ const AddPostCard = ({ setAddPost }) => {
     setAddedPhotos([...addedPhotos.filter((photo) => photo !== filename)]);
   }
 
-  // const numberOfPhotos = addedPhotos?.length;
+  const numberOfPhotos = addedPhotos?.length;
   return (
     <div className=" w-[90%] flex flex-col gap-4 mx-auto bg-white  rounded-md shadow-md p-4 lg:w-[35%]">
       <div className=" flex  items-center justify-between">
@@ -60,6 +66,7 @@ const AddPostCard = ({ setAddPost }) => {
           </svg>
         </button>
       </div>
+      {errMsg && <ErrorCard message={errMsg} />}
       <div className=" w-full">
         <textarea
           placeholder="Write something"
@@ -68,10 +75,10 @@ const AddPostCard = ({ setAddPost }) => {
           className=" p-2 w-full text-sm md:text-base bg-transparent outline-none placeholder:font-light placeholder:text-gray-400"
         ></textarea>
 
-        <div className=" grid grid-cols-2 gap-2">
-          {addedPhotos.map((link) => {
+        {/* <div className=" grid grid-cols-2 gap-2">
+          {addedPhotos.map((link, index) => {
             return (
-              <div className=" h-full flex relative ">
+              <div key={index} className=" h-full flex relative ">
                 <img
                   className=" w-full h-full  object-cover "
                   src={link}
@@ -100,42 +107,45 @@ const AddPostCard = ({ setAddPost }) => {
               </div>
             );
           })}
-        </div>
+        </div> */}
 
-        {/* {numberOfPhotos === 1 &&
-          addedPhotos.map((link, index) => (
-            <div className=" h-40 flex relative " key={index}>
-              {console.log(index)}
-              <img
-                className=" w-full h-full  object-cover "
-                src={link}
-                alt=""
-              />
-              <button
-                onClick={() => removePhoto(link)}
-                className=" absolute bottom-1 right-1 bg-black py-2 px-2 lg:px-3 cursor-pointer bg-opacity-50 text-white rounded-2xl"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-4 lg:w-6 h-4 lg:h-6"
+        {numberOfPhotos === 1 && (
+          <div className=" overflow-hidden rounded-lg">
+            {addedPhotos.map((link, index) => (
+              <div className=" h-full flex relative " key={index}>
+                {console.log(index)}
+                <img
+                  className=" w-full h-full  object-cover "
+                  src={link}
+                  alt=""
+                />
+                <button
+                  onClick={() => removePhoto(link)}
+                  className=" absolute bottom-1 right-1 bg-black py-2 px-2 lg:px-3 cursor-pointer bg-opacity-50 text-white rounded-2xl"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                  />
-                </svg>
-              </button>
-            </div>
-          ))}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-4 lg:w-6 h-4 lg:h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
         {numberOfPhotos === 2 && (
-          <div className=" grid grid-cols-2 gap-2">
-            {addedPhotos.map((link) => (
-              <div className=" h-full flex relative ">
+          <div className=" grid grid-cols-2 gap-2 overflow-hidden rounded-lg">
+            {addedPhotos.map((link, index) => (
+              <div key={index} className=" h-48 flex relative ">
                 <img
                   className=" w-full h-full  object-cover "
                   src={link}
@@ -164,7 +174,124 @@ const AddPostCard = ({ setAddPost }) => {
               </div>
             ))}
           </div>
-        )} */}
+        )}
+        {numberOfPhotos === 3 && (
+          <div className="grid gap-2  grid-cols-[2fr_1fr]  rounded-lg overflow-hidden ">
+            <div className=" h-full flex relative ">
+              <img
+                className=" w-full h-full  object-cover "
+                src={addedPhotos[0]}
+                alt=""
+              />
+              <button
+                onClick={() => removePhoto(addedPhotos[0])}
+                className=" absolute bottom-1 right-1 bg-black py-2 px-2 lg:px-3 cursor-pointer bg-opacity-50 text-white rounded-2xl"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-4 lg:w-6 h-4 lg:h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div className=" h-full flex relative ">
+                <img
+                  className=" w-full h-full  object-cover "
+                  src={addedPhotos[1]}
+                  alt=""
+                />
+                <button
+                  onClick={() => removePhoto(addedPhotos[1])}
+                  className=" absolute bottom-1 right-1 bg-black py-2 px-2 lg:px-3 cursor-pointer bg-opacity-50 text-white rounded-2xl"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-4 lg:w-6 h-4 lg:h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className=" h-full flex relative ">
+                <img
+                  className=" w-full h-full  object-cover "
+                  src={addedPhotos[2]}
+                  alt=""
+                />
+                <button
+                  onClick={() => removePhoto(addedPhotos[2])}
+                  className=" absolute bottom-1 right-1 bg-black py-2 px-2 lg:px-3 cursor-pointer bg-opacity-50 text-white rounded-2xl"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-4 lg:w-6 h-4 lg:h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        {numberOfPhotos === 4 && (
+          <div className=" grid grid-cols-2  gap-2 overflow-hidden rounded-lg">
+            {addedPhotos.map((link, index) => (
+              <div key={index} className=" h-48 flex relative ">
+                <img
+                  className=" w-full h-full  object-cover "
+                  src={link}
+                  alt=""
+                />
+                {console.log(link)}
+                <button
+                  onClick={() => removePhoto(link)}
+                  className=" absolute bottom-1 right-1 bg-black py-2 px-2 lg:px-3 cursor-pointer bg-opacity-50 text-white rounded-2xl"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-4 lg:w-6 h-4 lg:h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
 
         {/* <PhotoGrid addedPhotos={addedPhotos} setAddedPhotos={setAddedPhotos} /> */}
         {/* <div className=" grid grid-cols-2 gap-3 rounded-2xl overflow-hidden">
