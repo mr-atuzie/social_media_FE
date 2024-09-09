f1866656be278efaa30f023fc314e684205cbf6b
mr-atuzie <user email>
2024-09-09 12:18:27 +0100

    search

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -91,12 +91,36 @@ const Profile = () => {
                     alt=""
                     className=" h-full w-full bg-gray-400 rounded-md object-cover"
                   />
-                  <div className=" ">
+                  <div className=" relative ">
                     <img
                       src={user?.avatar}
                       alt=""
                       className=" w-24 lg:w-32 h-24 lg:h-32 absolute left-0 right-0 m-auto -bottom-16 bg-gray-400 rounded-full object-cover ring-4 ring-white z-20"
                     />
+                    <label
+                      htmlFor="myImage"
+                      className=" cursor-pointer  absolute bottom-0 right-2 p-2 h-10 w-10 rounded-full bg-black text-white"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
+                        />
+                      </svg>
+                    </label>
                   </div>
                 </div>
                 <h1 className=" mt-20  font-medium text-2xl">
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileCard from "../components/ProfileCard";
 import Sidebar from "../components/Sidebar";
 import RightMenu from "../components/RightMenu";
 import FriendRequests from "../components/FriendRequests";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 const Search = () => {
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+
+  const searchUser = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!search) {
+      toast.error("Enter search field");
+    }
+
+    try {
+      const { data } = await axios.post("/api/v1/user/search", {
+        username: search,
+      });
+
+      setLoading(false);
+      console.log(data);
+
+      if (data.length < 1) {
+        setNotFound(true);
+      }
+      setUsers(data);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      toast.error(message);
+      setLoading(false);
+    }
+  };
+  const loader = [1, 2];
   return (
     <div className=" flex gap-6 pt-6">
       <div className="hidden lg:flex flex-col gap-6 w-[25%]">
@@ -14,20 +54,65 @@ const Search = () => {
       <div className="w-full md:w-[70%] lg:w-[45%]">
         <div className=" flex-col gap-6 flex">
           <div className=" bg-white rounded-lg p-4 shadow-md">
-            <div>
-              {/* <label className=" text-sm" htmlFor="username">
-                Search
-              </label> */}
+            <form onSubmit={searchUser}>
               <input
                 className=" border rounded-md p-2 w-full placeholder:text-sm placeholder:font-normal bg-gray-50"
                 type="text"
                 id="username"
                 name="username"
-                placeholder="Search username"
-                // value={username}
-                // onChange={(e) => setUsername(e.target.value)}
+                placeholder="Search  username"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
               />
-            </div>
+            </form>
+            {loading ? (
+              loader.map((l) => (
+                <div key={l} className="flex my-4">
+                  <div className=" w-10 h-10 rounded-full bg-gray-100"></div>
+                  <div className=" w-full flex-1 ">
+                    <div className=" font-semibold text-center w-[80%]  bg-gray-100 p-2"></div>
+                    <div className=" flex items-center my-2 w-[40%] rounded-lg justify-center  bg-gray-100 p-2 text-gray-500  gap-2"></div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className=" my-4">
+                {users?.length > 0 &&
+                  users?.map((user) => {
+                    return (
+                      <div key={user?._id}>
+                        <div className="  flex justify-between items-center  text-sm">
+                          <div className=" flex items-center gap-4">
+                            <Link to={"/profile/" + user._id}>
+                              <div>
+                                <img
+                                  src={user?.avatar}
+                                  alt=""
+                                  className=" w-10 h-10 object-cover  rounded-full"
+                                />
+                              </div>
+                            </Link>
+
+                            <div className=" flex flex-col">
+                              <span className=" font-medium">{user?.name}</span>
+                              <span className=" text-gray-500 text-xs">
+                                @{user?.username}
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <hr className=" border-t  border-gray-100 mt-2 w-[90%] rounded-lg self-center" />
+                      </div>
+                    );
+                  })}
+
+                {notFound && (
+                  <div className=" text-gray-500  my-4 text-sm capitalize ">
+                    No user found
+                  </div>
+                )}
+              </div>
+            )}
           </div>
           <FriendRequests />
         </div>
