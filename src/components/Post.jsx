fa4eeac8847e6149545f917d08f7e8591418065b
rex atuzie <rexatuzie@gmail.com>
2024-10-22 15:03:32 +0100

    post tooltip

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import Tooltip from "rc-tooltip";
+// import Tooltip from "rc-tooltip";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
 import { selectUser } from "../redux/features/userSlice";
 import Images from "./Images";
-import "rc-tooltip/assets/bootstrap_white.css";
+// import "rc-tooltip/assets/bootstrap_white.css";
 const { formatDistanceToNow } = require("date-fns");
 
 const Post = (props) => {
@@ -14,6 +14,18 @@ const Post = (props) => {
   const currentUser = useSelector(selectUser);
 
   const [showAllPhotos, setShowAllPhotos] = useState(false);
+
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
+
+  // Show tooltip on mouse enter
+  const handleMouseEnter = () => {
+    setTooltipVisible(true);
+  };
+
+  // Hide tooltip on mouse leave
+  const handleMouseLeave = () => {
+    setTooltipVisible(false);
+  };
 
   const liked = post?.likes.includes(currentUser?._id);
 
@@ -89,28 +101,55 @@ const Post = (props) => {
               </p>
             </div>
           </div>
-          <Tooltip
-            placement="bottomRight'"
-            trigger={["click"]}
-            overlay={<span>tooltip</span>}
-          >
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                />
-              </svg>
-            </button>
-          </Tooltip>
+          {currentUser?._id === post?.user._id && (
+            <div
+              className="relative"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              {/* Trigger button */}
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                  />
+                </svg>
+              </button>
+
+              {/* Tooltip content */}
+              {isTooltipVisible && (
+                <div className="absolute  bg-white shadow-sm text-xs lg:text-sm border  rounded p-2.5  right-4  top-4 mb-2 whitespace-nowrap">
+                  <button className="flex items-center gap-2 text-red-400">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-4 "
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+
+                    <span className=" font-medium">Delete </span>
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
         </div>
 
         <Link to={"/post/" + post?._id}>
