03ea71dba75f9a18d89ce029ac12c22155a3d69c
rex atuzie <rexatuzie@gmail.com>
2024-10-26 12:25:06 +0100

    add post card --

--- src/components/AddPostCard.jsx
+++ src/components/AddPostCard.jsx
@@ -119,11 +119,25 @@ const AddPostCard = ({ setAddPost }) => {
             alt=""
             className=" w-8  h-8 rounded-full object-cover"
           />
-          <div className=" ">
+          <div className=" flex gap-1 items-center ">
             <p className=" font-medium text-sm lg:text-base">{user?.name}</p>
-            <p className=" text-gray-500 text-xs -mt-1 lg:text-sm">
-              @{user?.username}
-            </p>
+
+            {user?.verified && (
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-5 text-green-500"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            )}
           </div>
         </div>
 
@@ -152,7 +166,7 @@ const AddPostCard = ({ setAddPost }) => {
           value={desc}
           // rows={4}
           onChange={(e) => setDesc(e.target.value)}
-          className="w-full p-4 text-gray-800  rounded-lg focus:outline-none  shadow-sm transition duration-200 ease-in-out resize-none customScrollBar"
+          className="w-full p-4 text-gray-800  rounded-lg focus:outline-none   transition duration-200 ease-in-out resize-none customScrollBar"
           // className=" p-2 w-full min-h-20 text-sm md:text-base bg-transparent outline-none  placeholder:font-light placeholder:text-gray-400"
         ></textarea>
 
